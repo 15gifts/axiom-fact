9e14b53c6117f14a8030b7f19520aad053f70c11
FabrizioTrovato <56155570+FabrizioTrovato@users.noreply.github.com>
2024-05-10 15:41:39 +0100

    API'd

---
+++ app/api/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Request
+
+from typing import List
+
+import os
+import sys
+sys.path.append('..')
+from alignscore import AlignScore
+
+app = FastAPI()
+
+@app.on_event('startup')
+async def startup():
+    print('Starting up...')
+    app.scorer = AlignScore(
+        model='roberta-base',
+        batch_size=32,
+        device='cpu',
+        ckpt_path=os.path.join('app/models/AlignScore-base.ckpt'),
+        evaluation_mode='nli_sp',
+        verbose=False
+    )
+
+@app.post('/')
+def score_claim(
+    request: Request,
+    contexts: List[str],
+    claims: List[str]
+):
+    return {
+        'score': request.app.scorer.score(
+            contexts=contexts,
+            claims=claims
+        )
+        }
